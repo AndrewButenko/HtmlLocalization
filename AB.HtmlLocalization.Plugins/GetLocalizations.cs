25ab33ebea9453f20ed692d1e9a70c867c0633ae
Andrii Butenko <andrewbutenko@outlook.com>
2018-12-17 23:13:36 -0500

    Import Project

---
+++ AB.HtmlLocalization.Plugins/GetLocalizations.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace AB.HtmlLocalization.Plugins
+{
+    public class GetLocalizations : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            var factory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = factory.CreateOrganizationService(null);
+
+            var webResourcesString = (string)context.InputParameters["WebResources"];
+
+            var webResources = (JsonConvert.DeserializeObject<List<string>>(webResourcesString)).ToArray<object>();
+
+            var jsWebResourcesQuery = new QueryExpression("webresource")
+            {
+                ColumnSet = new ColumnSet("dependencyxml")
+            };
+
+            jsWebResourcesQuery.Criteria.AddCondition("name", ConditionOperator.In, webResources);
+            jsWebResourcesQuery.Criteria.AddCondition("dependencyxml", ConditionOperator.NotNull);
+
+            var jsWebResources = service.RetrieveMultiple(jsWebResourcesQuery).Entities.ToList();
+
+            var dependentWebresources = new List<string>();
+
+            jsWebResources.ForEach(t =>
+            {
+                var dependencyXml = t.GetAttributeValue<string>("dependencyxml");
+
+                var xDocument = XDocument.Parse(dependencyXml);
+
+                dependentWebresources.AddRange(xDocument.XPathSelectElements("Dependencies/Dependency/Library")
+                    .Where(d => d.Attribute("name") != null).Select(d => d.Attribute("name").Value).Distinct());
+            });
+
+            var resxWebResourcesQuery = new QueryExpression("webresource")
+            {
+                ColumnSet = new ColumnSet("content", "name")
+            };
+            resxWebResourcesQuery.Criteria.AddCondition("name", ConditionOperator.In, dependentWebresources.ToList<object>().ToArray());
+
+            var resxWebResources = service.RetrieveMultiple(resxWebResourcesQuery).Entities.ToList();
+
+            var dependencyNameToGuidMap = new Dictionary<string, string>();
+
+            var serializedLabels = new Dictionary<string, string>();
+
+            resxWebResources.ForEach(t =>
+            {
+                dependencyNameToGuidMap.Add(t.GetAttributeValue<string>("name"), t.Id.ToString("N").ToUpper());
+
+                var content = t.GetAttributeValue<string>("content");
+
+                content = Encoding.Default.GetString(Convert.FromBase64String(content)).Substring(3);
+
+                var contentDocument = XDocument.Parse(content);
+
+                var dataNodes = contentDocument.XPathSelectElements("root/data").Select(n => new { Name = n.Attribute("name").Value, Value = n.Element("value").Value }).ToDictionary(n => n.Name, n => n.Value);
+
+                var labels = JsonConvert.SerializeObject(dataNodes);
+
+                serializedLabels.Add($"LOCID_{t.Id:N}".ToUpper(), labels);
+            });
+
+            serializedLabels.Add("DependencyNameToGuidMap", JsonConvert.SerializeObject(dependencyNameToGuidMap));
+
+            context.OutputParameters["Localizations"] = JsonConvert.SerializeObject(serializedLabels);
+        }
+    }
+
+}
---
+++ AB.HtmlLocalization.WebResources/ab_/HtmlLocalization.js
@@ -0,0 +1,52 @@
+﻿var AB = AB || {};
+
+AB.HtmlLocalization = (function () {
+    function initialize(webResources) {
+        return new Promise(function(resolve, reject) {
+
+            var ab_GetLocalizationRequest = {
+                WebResources: JSON.stringify(webResources),
+                getMetadata: function() {
+                    return {
+                        boundParameter: null,
+                        parameterTypes: {
+                            "WebResources": {
+                                "typeName": "Edm.String",
+                                "structuralProperty": 1
+                            }
+                        },
+                        operationType: 0,
+                        operationName: "ab_GetLocalization"
+                    };
+                }
+            };
+
+            Xrm.WebApi.online.execute(ab_GetLocalizationRequest).then(
+                function success(result) {
+                    if (result.ok) {
+                        var results = JSON.parse(result.responseText);
+
+                        var resultsObject = JSON.parse(results.Localizations);
+
+                        for (var i in resultsObject) {
+                            if (i === "DependencyNameToGuidMap") {
+                                window[i] = JSON.parse(resultsObject[i]);
+                            } else {
+                                window[i] = resultsObject[i];
+                            }
+                        }
+                    }
+
+                    resolve();
+                },
+                function (error) {
+                    reject(error);
+                }
+            );
+        });
+    }
+
+    return {
+        Initialize: initialize
+    };
+})();
